1a1e6ec2c5bf2021a6f0c5b3b8761dc02d267a7b
serg-by <sergmolby@gmail.com>
2022-07-10 12:20:34 +0300

    create new branch
    Create new branch. File change index.js

---
+++ components/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Container = ({children, className=""}) => {
+    return (
+        <div
+            className={`container mx-auto px-5 ${className}`}
+
+        >{children}
+        </div>
+    )
+}
+
+export default Container
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-
+import Container from "../components/Container";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-  
-
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      <h1 className="text-6xl font-bold">
-        Welcome to{' '}
-        <a className="text-blue-600" href="https://nextjs.org">
-          Next.js!
-        </a>
-      </h1>         
-    </main>
-
-   
-  </div>
-  )
+    <>
+      <div  className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Container>
+         <div className="text-center">
+            <h1 className="text-6xl font-bold">
+              Welcome to{" "}
+              <a className="text-blue-600" href="https://nextjs.org">
+                Next.js!
+              </a>
+            </h1>
+         </div>
+        </Container>
+      </div>      
+    </>
+  );
 }
